d1ab621fdd1210ad8149db0c5e0f95338cf2d019
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-11 17:05:57 +0200

    Set Up the Facade Pattern: implementing LibraryAPI Facade Pattern

--- MusicApp_Design Pattern/API/LibraryAPI/LibraryAPI.swift
+++ MusicApp_Design Pattern/API/LibraryAPI/LibraryAPI.swift
@@ -9,10 +9,33 @@
 import Foundation
 
 final class LibraryAPI {
-  // 1
-  static let shared = LibraryAPI()
-  // 2
-  private init() {
+    // 1
+    static let shared = LibraryAPI()
+    // 2
+    private let persistencyManager = PersistencyManager()
+    private let httpClient = HTTPClient()
+    private let isOnline = false
+    // 3
+    private init() {
+        
+    }
+    
+    func getAlbums() -> [Album] {
+      return persistencyManager.getAlbums()
+    }
+      
+    func addAlbum(_ album: Album, at index: Int) {
+      persistencyManager.addAlbum(album, at: index)
+      if isOnline {
+        httpClient.postRequest("/api/addAlbum", body: album.description)
+      }
+    }
+      
+    func deleteAlbum(at index: Int) {
+      persistencyManager.deleteAlbum(at: index)
+      if isOnline {
+        httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+      }
+    }
 
-  }
 }
